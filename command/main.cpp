4c88032a60d583cba4755802316acc23f3679235
itroyen <inso4ka@mail.ru>
2023-08-08 14:26:54 +0300

    added -b

--- command/main.cpp
+++ command/main.cpp
@@ -15,15 +15,16 @@ class DiskAnalyzer
         } else {
             processFile(m_rootPath, totalBlocks);
         }
-        if (m_size) {
-            std::cout << "Total size: " << totalBlocks << " blocks" << std::endl;
+        if (flag.m_size) {
+            LOG_INFO(std::to_string(totalBlocks) + "\t" + m_rootPath.string());
         }
     }
 
-    void setFlags(bool size, bool showData)
+    void setFlags(bool size, bool showData, bool bytes)
     {
-        m_size     = size;
-        m_showData = showData;
+        flag.m_size     = size;
+        flag.m_showData = showData;
+        flag.m_bytes    = bytes;
     }
 
   private:
@@ -31,10 +32,14 @@ class DiskAnalyzer
     {
         try {
             std::uintmax_t fileSize = std::filesystem::file_size(filePath);
-            std::uintmax_t blocks   = (fileSize + 511) / 512; // Размер файла в блоках
-            totalBlocks += blocks;
-            if (m_showData) {
-                LOG_INFO(getFileSizeInfo(filePath, blocks));
+            if (flag.m_bytes) {
+                LOG_INFO(getFileSizeInfo(filePath, fileSize));
+            } else {
+                std::uintmax_t blocks = (fileSize + 511) / 512;
+                totalBlocks += blocks;
+                if (flag.m_showData) {
+                    LOG_INFO(getFileSizeInfo(filePath, blocks));
+                }
             }
 
         } catch (const std::filesystem::filesystem_error& e) {
@@ -46,7 +51,7 @@ class DiskAnalyzer
     {
         try {
             if (std::filesystem::is_empty(directoryPath)) {
-                totalBlocks += 1; // Учитываем пустую папку размером в 1 блок
+                totalBlocks += 1;
             } else {
                 for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
                     if (entry.is_directory()) {
@@ -61,28 +66,33 @@ class DiskAnalyzer
         }
     }
 
-    std::string getFileSizeInfo(const std::filesystem::path& filePath, const std::uintmax_t blocks) const
+    std::string getFileSizeInfo(const std::filesystem::path& filePath, const std::uintmax_t size) const
     {
         std::uintmax_t fileSize = std::filesystem::file_size(filePath);
-        return std::to_string(blocks) + "\t" + filePath.string();
+        return std::to_string(size) + "\t" + filePath.string();
     }
 
   private:
     std::filesystem::path m_rootPath;
-    bool                  m_size;
-    bool                  m_showData;
+    struct flags
+    {
+        bool m_size;
+        bool m_showData;
+        bool m_bytes;
+    };
+    flags flag;
 };
 
-std::string buildCommand(int argc, char** argv, bool& size, bool& showData)
+std::string buildCommand(int argc, char** argv, bool& size, bool& showData, bool& bytes)
 {
     if (argc == 2) {
         return std::string(argv[1]);
     } else if (argc == 3) {
         std::string option(argv[1]);
-        if (option == "-m") {
-            return "du -m " + std::string(argv[2]);
-        } else if (option == "-g") {
-            return "du -g " + std::string(argv[2]);
+        if (option == "-b") {
+            bytes = true;
+            return std::string(argv[2]);
+
         } else if (option == "-s") {
             showData = false;
             size     = true;
@@ -99,15 +109,16 @@ int main(int argc, char** argv)
 {
     bool size     = false;
     bool showData = true;
+    bool bytes    = false;
 
-    std::string command = buildCommand(argc, argv, size, showData);
+    std::string command = buildCommand(argc, argv, size, showData, bytes);
     if (command.empty()) {
         LOG_CRITICAL("Usage: du [-m|-g|-s|-b] <path>");
         return 1;
     }
 
     DiskAnalyzer analyzer(command);
-    analyzer.setFlags(size, showData);
+    analyzer.setFlags(size, showData, bytes);
     analyzer.analyze();
 
     return 0;
